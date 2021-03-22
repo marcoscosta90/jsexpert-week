4bb984ca3e4c36df7cddb347688478731f547da9
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-03-22 15:19:50 -0300

    Starting js expert week

---
+++ client/src/terminalController.js
@@ -0,0 +1,26 @@
+import ComponentsBuilder from "./components.js"
+
+export default class TerminalController {
+    constructor() { }
+
+    #onInputReceived(eventEmitter) {
+        return function() {
+            const message = this.getValue();
+            console.log(message)
+            this.clearValue()
+        }
+    }
+
+
+    async initializeTable(eventEmitter) {
+        const components = new ComponentsBuilder()
+            .setScreen({title: 'HackerChat - Marcos Costa'})
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .setChatComponent()
+            .build()
+
+        components.input.focus()
+        components.screen.render()
+    }
+}
